37552c415300fa3c7ae6a85bc56bd845f20d78ed
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-20 20:16:25 -0500

    Toggle done/checked item

--- scripts.js
+++ scripts.js
@@ -27,6 +27,17 @@ function addItem(e) {
     }).join('');
   }
 
+  function toggleDone(e) {
+    // Skip unless it's an input
+    if (!e.target.matches('input')) return;
+    const el = e.target;
+    const index = el.dataset.index;
+    items[index].done =!items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+  }
+
  addItems.addEventListener('submit', addItem);
+ itemsList.addEventListener('click', toggleDone);
 
  populateList(items, itemsList);
